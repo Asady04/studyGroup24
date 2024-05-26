415cc7e949313ec9f0361ba1b745069d504531d7
Asady <fayyadh.asady04@gmail.com>
2024-05-26 15:47:23 +0700

    player done

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 5f;
+    public Animator aniPlayer;
+    public Animator aniBoost;
+    private Rigidbody2D rb;
+    public GameObject PlayerBullet;
+    public GameObject BulletPos;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = (GameObject)Instantiate(PlayerBullet);
+            bullet.transform.position = BulletPos.transform.position;
+        }
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb.velocity = movement * speed;
+
+        if (movement == Vector2.right)
+        {
+            aniPlayer.SetBool("isRight", true);
+            aniPlayer.SetBool("isLeft", false);
+            aniPlayer.SetBool("isIdle", false);
+            aniBoost.SetBool("isRight", true);
+            aniBoost.SetBool("isLeft", false);
+            aniBoost.SetBool("isIdle", false);
+        }
+        else if (movement == Vector2.left)
+        {
+            aniPlayer.SetBool("isLeft", true);
+            aniPlayer.SetBool("isIdle", false);
+            aniPlayer.SetBool("isRight", false);
+            aniBoost.SetBool("isLeft", true);
+            aniBoost.SetBool("isIdle", false);
+            aniBoost.SetBool("isRight", false);
+        }
+        else
+        {
+            aniPlayer.SetBool("isIdle", true);
+            aniPlayer.SetBool("isLeft", false);
+            aniPlayer.SetBool("isRight", false);
+            aniBoost.SetBool("isIdle", true);
+            aniBoost.SetBool("isLeft", false);
+            aniBoost.SetBool("isRight", false);
+        }
+    }
+}
